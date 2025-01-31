08997bda8ce5d755b05a6def321401cc0642c6ad
yingyingyingqwq <yingyingyingqwp@gmail.com>
2025-02-01 01:30:44 +0800

    Github Pages Build

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   modules: ['@pinia/nuxt'],
   css: ['~/assets/css/main.css'],
-  plugins: ['~/plugins/dataLoader'],
+  plugins: ['~/plugins/dataLoader.js'],
   compatibilityDate: '2025-01-28',
   nitro: {
     static: true
