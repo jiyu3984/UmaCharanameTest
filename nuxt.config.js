fa40b36355bc09ec0c21da0d02fc42bb543c4c6a
yingyingyingqwq <yingyingyingqwp@gmail.com>
2025-02-01 01:58:34 +0800

    添加GA

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,5 +5,24 @@ export default {
   compatibilityDate: '2025-01-28',
   nitro: {
     static: true
+  },
+  app: {
+    head: {
+      script: [
+        {
+          src: `https://www.googletagmanager.com/gtag/js?id=G-F57HL0RLZB`,
+          async: true
+        },
+        {
+          innerHTML: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-F57HL0RLZB');
+          `,
+          type: 'text/javascript'
+        }
+      ]
+    }
   }
 };
